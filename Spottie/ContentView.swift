d988c416b0f17efdc27b6569ee2d5795edb1fcae
Lee Jun Kit <junkit@22media.co>
2021-05-20 16:36:00 +0800

    Initial Commit

---
+++ Spottie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spottie
+//
+//  Created by Lee Jun Kit on 20/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spottie/SpottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpottieApp.swift
+//  Spottie
+//
+//  Created by Lee Jun Kit on 20/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SpottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
